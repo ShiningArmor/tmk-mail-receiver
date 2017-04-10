844d229ccc99722e6c41ebea9bcc710d5bdf54a9
6stone@gmail.com <66stone@gmail.com>
2017-04-10 19:18:05 -0300

    first commit

---
+++ models.py
@@ -0,0 +1,34 @@
+import peewee
+import json
+settings = json.loads(open("settings.json").read())
+db = peewee.MySQLDatabase(settings["database"], 
+                            user=settings["user"],
+                            passwd=settings["password"],
+                            host=settings["host"],
+                            port=settings["port"])
+
+class eg_email(peewee.Model):
+    id = peewee.IntegerField()
+    creado_por = peewee.IntegerField()
+    asignado_a = peewee.IntegerField()
+    asunto = peewee.CharField()
+    detalle = peewee.TextField()
+    e_mail = peewee.TextField()
+    creado_el = peewee.DateTimeField()
+    fecha_hora_inicio = peewee.DateTimeField()
+    actividad = peewee.CharField()
+    estado = peewee.CharField()
+    class Meta:
+        database = db
+
+class eg_cuenta_de_email(peewee.Model):
+    id = peewee.IntegerField()
+    nombre = peewee.CharField()
+    email = peewee.CharField()
+    clave_email = peewee.CharField()
+    servidor_pop3 = peewee.CharField()
+    puerto_pop3 = peewee.CharField()
+    activo = peewee.BooleanField()
+    lista_blanca = peewee.TextField()
+    class Meta:
+        database = db
---
+++ recibir_email.py
@@ -0,0 +1,174 @@
+from datetime import datetime
+import time
+now = datetime.now()
+
+import smtplib
+import poplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.Header import Header
+from email.Utils import parsedate, parseaddr, formataddr
+from email.parser import Parser
+from email.header import decode_header
+import os
+
+from models import eg_actividad as NotificadorExterno, eg_cuenta_de_email as configuracionBPM
+
+def recibir_email(config):
+    # Se establece conexion con el servidor pop3
+    m = poplib.POP3(config.servidor_pop3 , str(config.puerto_pop3))
+    m.user(config.email)
+    m.pass_(config.clave_email)
+    #contar mails sin leer
+    numero = len(m.list()[1])
+    print (numero)
+    #obtener los mensajes para analizarlos
+    for i in range (numero):
+        print "Mensaje numero"+str(i+1)
+        print "--------------------"
+        # Se lee el mensaje y se parsea el mje
+        response, headerLines, bytes = m.retr(i+1)
+        mensaje='\n'.join(headerLines)
+        p = Parser()
+        email = p.parsestr(mensaje)
+        print (parseaddr(email['From'])[1])
+        remitentes = config.lista_blanca.split(',')
+        print remitentes
+        for remitente in remitentes:
+            try:
+                print (parseaddr(email['From'])[1].index(remitente))
+                if parseaddr(email['From'])[1].index(remitente)>=0:
+                    #Se formatea la hora para guardarla en la BD
+                    a = parsedate(email['Date'])
+                    b = time.mktime(a)
+                    c = datetime.fromtimestamp(int(b)).strftime('%Y-%m-%d %H:%M')
+                    #''.join([ unicode(t[0], t[1] or default_charset) for t in dh ])
+                    #Si es compuesto con HTML o texto plano ascii
+                    default_charset = 'ascii'
+                    tit = decode_header(email['Subject'])
+                    tipo_mail = tit[0][1]
+                    if tit[0][1]=='utf-8' or tit[0][1]==None:
+                        tit = ''.join([ unicode(t[0], t[1] or default_charset) for t in tit ])
+                        es_utf8= True
+                        for part in email.walk():
+                            parte=''
+                            if part.get_content_type()=="text/html":
+                                print 'html'
+                                part = sanitise(part)
+                                parte = part.get_payload(decode=True).decode(part.get_content_charset())
+                            else:
+                                print 'Texto plano'
+                                part = sanitise(part)
+                                parte = part.get_payload(decode=True)
+                    else:
+                        for part in email.walk():
+                            parte=''
+                            if part.get_content_type()=="text/html":
+                                print 'html'
+                                part = sanitise(part)
+                                parte = part.get_payload(decode=True)
+                            else:
+                                print 'Texto plano'
+                                part = sanitise(part)
+                                parte = part.get_payload(decode=True)
+
+                    #Se guarda una instancia del Mail
+                    cuenta = config.nombre
+                    noti = NotificadorExterno()
+                    noti.creado_por =1
+                    noti.asignado_a = 8
+                    noti.creado_el = datetime.now().strftime('%Y-%m-%d %H:%M')
+                    noti.fecha_hora_inicio = noti.creado_el
+                    noti.asunto = tit[:40]
+                    noti.actividad = 'Email'
+                    noti.estado = 'Iniciada'
+                    noti.detalle = "- ['Remitente:" +parseaddr(email['From'])[1] + "', '1','" + datetime.now().strftime('%Y-%m-%d %H:%M') +"']" 
+                    noti.e_mail = parte
+                    
+                    try:
+                        '''if NotificadorExterno.select().where():
+                            print 'YA EXISTE'
+                        else:'''
+                        noti.save()
+                        print noti.id
+                        print 'Se grabo!!!'
+                    except Exception, e: 
+                        print ('NO SE GRABO!!!!')
+                        print repr(e)
+            except Exception, e:
+                    print e
+
+    poplist = m.list()
+    if poplist[0].startswith('+OK') :
+        msglist = poplist[1]
+        for msgspec in msglist :
+            # msgspec is something like "3 3941", 
+            # msg number and size in octets
+            msgnum = int(msgspec.split(' ')[0])
+            print "Deleting msg %d\r" % msgnum,
+            m.dele(msgnum)
+        else :
+        	print "No messages for"
+    else :
+        print "Couldn't list messages: status", poplist[0]
+    m.quit()	
+#clase para filtrado de EMail por tipo de documentos adjuntos
+
+import re
+BAD_CONTENT_RE = re.compile('application/(msword|msexcel)', re.I)
+BAD_FILEEXT_RE = re.compile(r'(\.exe|\.zip|\.pif|\.scr|\.ps|\.doc|\.xls|\.docx|\.xlsx)$')
+
+def sanitise(msg):
+    # Strip out all payloads of a particular type
+    ct = msg.get_content_type()
+    # We also want to check for bad filename extensions
+    fn = msg.get_filename()
+    # get_filename() returns None if there's no filename
+    if BAD_CONTENT_RE.search(ct) or (fn and BAD_FILEEXT_RE.search(fn)):
+        # Ok. This part of the message is bad, and we're going to stomp
+        # on it. First, though, we pull out the information we're about to
+        # destroy so we can tell the user about it.
+
+        # This returns the parameters to the content-type. The first entry
+        # is the content-type itself, which we already have.
+        params = msg.get_params()[1:]
+        # The parameters are a list of (key, value) pairs - join the
+        # key-value with '=', and the parameter list with ', '
+        params = ', '.join([ '='.join(p) for p in params ])
+        # Format up the replacement text, telling the user we ate their
+        # email attachment.
+        replace = ReplaceString % dict(content_type=ct,
+                                       filename=fn,
+                                       params=params)
+        # Install the text body as the new payload.
+        msg.set_payload(replace)
+        # Now we manually strip away any paramaters to the content-type
+        # header. Again, we skip the first parameter, as it's the
+        # content-type itself, and we'll stomp that next.
+        for k, v in msg.get_params()[1:]:
+            msg.del_param(k)
+        # And set the content-type appropriately.
+        msg.set_type('text/plain')
+        # Since we've just stomped the content-type, we also kill these
+        # headers - they make no sense otherwise.
+        del msg['Content-Transfer-Encoding']
+        del msg['Content-Disposition']
+    else:
+        # Now we check for any sub-parts to the message
+        if msg.is_multipart():
+            # Call the sanitise routine on any subparts
+            payload = [ sanitise(x) for x in msg.get_payload() ]
+            # We replace the payload with our list of sanitised parts
+            msg.set_payload(payload)
+    # Return the sanitised message
+    return msg
+
+
+def main():
+    cuentas = configuracionBPM.select().where(configuracionBPM.activo==True)
+    for cta in cuentas:
+        recibir_email(cta)
+        
+
+if __name__ == '__main__':
+    main()
