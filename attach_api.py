3f3f5f8ed4bf7c3d6d2aa05f6708232695cef415
mario <malopez@vates.com>
2017-06-09 11:07:31 -0300

    negrada reconnect

--- attach_api.py
+++ attach_api.py
@@ -1,6 +1,6 @@
 from flask import request, url_for
 from flask import send_from_directory
-from models import eg_email as NotificadorExterno, eg_cuenta_de_email as configuracionBPM, email_adjunto_api as AttachAPI
+from models import eg_email as NotificadorExterno, eg_cuenta_de_email as configuracionBPM, email_adjunto_api as AttachAPI, db
 import json
 import logging
 import logging.handlers
